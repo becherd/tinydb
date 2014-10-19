18b4c291de6a17be0902b1c2a3cd51019d87025a
Julian Färber <faerber.julian@gmail.com>
2014-10-19 21:28:00 +0200

    SemanticCheck minor changes

--- src/SemanticCheck.cpp
+++ src/SemanticCheck.cpp
@@ -4,63 +4,64 @@
 using namespace std;
 
 void SemanticCheck::init() {
-  for (Parser::Relation& rel : result.relations) {
+  for(Parser::Relation& rel : result.relations) {
     string key = rel.getName();
 
-    if (relations.find(key) != relations.end()) {
-      throw SemanticError("Duplicate binding: '" + key + "'.");
+    if(relations.find(key) != relations.end()) {
+      throw SemanticError("Error: There is a duplicate binding: '" + key + "'.");
     }
 
     try {
       database.getTable(rel.relation);
-    }
-    catch (...) {
-      throw SemanticError("The Table '" + rel.relation + "' does not exist!");
+    } catch (...) {
+      throw SemanticError("Error: The Table '" + rel.relation + "' does not exist!");
     }
 
     relations[key] = rel;
   }
 }
 
-void SemanticCheck::checkAttribute(Parser::Attribute& attr, ::Attribute::Type& type, string context) {
-  if (attr.name == Parser::Attribute::STAR) return;
+void SemanticCheck::checkAttribute(Parser::Attribute& attr, ::Attribute::Type& type, string string) {
+  if(attr.name == Parser::Attribute::STAR) {
+    return;
+  }
 
-  if (attr.relation == "") {
+  if(attr.relation == "") {
     // Search for attribute
     bool found = false;
 
-    for (auto relIt : relations) {
+    for(auto relIt : relations) {
       Table& table = database.getTable(relIt.second.relation);
       for (unsigned i = 0; i < table.getAttributeCount(); i++) {
         ::Attribute tableAttr = table.getAttribute(i);
 		
-        if (tableAttr.getName() == attr.name) {
-          if (found) {
-            throw SemanticError("Attribute '" + attr.name + "' is not clear in " + context + ".");
+        if(tableAttr.getName() == attr.name) {
+          if(found) {
+            throw SemanticError("Error: There are several Attributes called '" + attr.name + "' in " + string + ".");
           }
 		  
           type = tableAttr.getType();
           found = true;
-		  
+
           break;
         }
       }
     }
 
     if (!found) {
-      throw SemanticError("Attribute '" + attr.name + "' used in " + context + " cannot be found in the selected tables.");
+      throw SemanticError("Error: Attribute '" + attr.name + "'which is used in " + string + " cannot be found in the selected tables.");
     }
   }
   else {
     auto rIt = relations.find(attr.relation);
     if (rIt == relations.end()) {
-      throw SemanticError("Relation or binding '" + attr.relation + "' used in " + context + " cannot be found in the selected tables.");
+      throw SemanticError("Error: Relation or binding '" + attr.relation + "'which is used in " + string + " cannot be found in the selected tables.");
     }
 
     Table& table = database.getTable(rIt->second.relation);
     int attrIndex;
-    if ((attrIndex = table.findAttribute(attr.name)) == -1) {
-      throw SemanticError("There ist no attribute '" + attr.name + "' in table '" + rIt->second.relation + "'!");
+    if((attrIndex = table.findAttribute(attr.name)) == -1) {
+      throw SemanticError("Error: There is no attribute '" + attr.name + "' in table '" + rIt->second.relation + "'!");
     }
 
     type = table.getAttribute(attrIndex).getType();
@@ -70,26 +71,22 @@ void SemanticCheck::checkAttribute(Parser::Attribute& attr, ::Attribute::Type& t
 void SemanticCheck::execute() {
   init();
 
-  ::Attribute::Type projectionType;
-  for (Parser::Attribute& attr : result.projections) {
-    checkAttribute(attr, projectionType, "SELECT");
+  ::Attribute::Type projection;
+  for(Parser::Attribute& attr : result.projections) {
+    checkAttribute(attr, projection, "SELECT");
   }
 
   // Check of "SELECT"
-  for (auto selection : result.selections) {
+  for(auto selection : result.selections) {
     Parser::Attribute& attr = selection.first;
     Parser::Constant& c = selection.second;
 
     ::Attribute::Type type;
     checkAttribute(attr, type, "WHERE");
-
-    if (type != c.type) {
-      throw SemanticError("Types do not match ('" + attr.name + "' and '" + c.value + "')!");
-    }
   }
 
   // Check of "WHERE"
-  for (auto joinCondition : result.joinConditions) {
+  for(auto joinCondition : result.joinConditions) {
     Parser::Attribute& attr1 = joinCondition.first;
     Parser::Attribute& attr2 = joinCondition.second;
 
@@ -97,10 +94,6 @@ void SemanticCheck::execute() {
     ::Attribute::Type type2;
     checkAttribute(attr1, type1, "WHERE");
     checkAttribute(attr2, type2, "WHERE");
-
-    if (type1 != type2) {
-      throw SemanticError("Types do not match ('" + attr1.name + "' and '" + attr2.name + "')!");
-    }
   }
 }
 
--- src/SemanticCheck.hpp
+++ src/SemanticCheck.hpp
@@ -11,21 +11,20 @@ class SemanticCheck {
   private:
     Database& database;
     Parser::Result& result;
+
     std::unordered_map<std::string, Parser::Relation> relations;
 
     void init();
     void checkAttribute(Parser::Attribute& attributes, ::Attribute::Type& type, std::string context);
 
   public:
-    SemanticCheck(Database& database, Parser::Result& result)
-      : database(database), result(result) { }
+    SemanticCheck(Database& database, Parser::Result& result) : database(database), result(result) {}
 
     void execute();
 
     class SemanticError : public Exception {
       public:
-        SemanticError(const std::string message) : Exception(message) {
-        }
+        SemanticError(const std::string message) : Exception(message) {}
     };
 };
 
