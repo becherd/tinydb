628d96f3f79b8f2eaf5ffeddcbed4b8cf28b2b84
David Becher <david.becher@mytum.de>
2014-10-29 13:10:37 +0100

    finished implementation of ExecutionPlan

--- q1.sql
+++ q1.sql
@@ -1,3 +1,3 @@
-select s.name, s.semester, h.matrnr
-from studenten s, hoeren h
-where s.matrnr =   h.matrnr
+select s1.name
+from studenten s1, hoeren h1, hoeren h2, studenten s2
+where s2.name='Fichte' and s2.matrnr=h2.matrnr and h2.vorlnr=h1.vorlnr and h1.matrnr=s1.matrnr
--- src/cts/executionplan/ExecutionPlan.cpp
+++ src/cts/executionplan/ExecutionPlan.cpp
@@ -35,7 +35,6 @@ void ExecutionPlan::fillRegister() {
 
 	vector<SQLParser::RelationAttribute> allAttributes;
 
-
 	for (unsigned int i = 0; i < r.projections.size(); i++) {
 		SQLParser::RelationAttribute attribute = r.projections.at(i);
 		if (!vectorContainsAttribute(allAttributes, attribute)) {
@@ -70,11 +69,11 @@ void ExecutionPlan::fillRegister() {
 		vector<SQLParser::Relation> relations = r.relations;
 		unsigned int index = 0;
 		for (index = 0; index < relations.size(); index++) {
-			if (relations.at(index).binding.compare(attribute.relation)==0) {
+			if (relations.at(index).binding.compare(attribute.relation) == 0) {
 				break;
 			}
 		}
-		cout << allAttributes.at(i).getName() << endl;
+
 		attributes.push_back(
 				{ allAttributes.at(i), tableScans[index]->getOutput(
 						attribute.name) });
@@ -90,29 +89,37 @@ void ExecutionPlan::generateExecutionPlan() {
 	vector<unique_ptr<Operator>> query;
 
 	//copy base relations into query
-	for(unsigned int i=0; i<tableScans.size(); i++){
+	for (unsigned int i = 0; i < tableScans.size(); i++) {
 		query.push_back(move(tableScans.at(i)));
 	}
 
 
-	for(unsigned int i=0; i<r.selections.size(); i++){
-		//find tablescan
-		unsigned int index=0;
-		for(; index<r.relations.size(); index++){
-			if(r.selections.at(i).first.relation.compare(r.relations.at(index).binding) == 0){
+	//replace base relations by resulting tables after selection if there is a selection <attribute>='<constant>'
+	for (unsigned int i = 0; i < r.selections.size(); i++) {
+		//find tablescan using the binding
+		unsigned int index = 0;
+		for (; index < r.relations.size(); index++) {
+			if (r.selections.at(i).first.relation.compare(
+					r.relations.at(index).binding) == 0) {
 				break;
 			}
 		}
 
 
+
+
 		unique_ptr<Operator> tableS(move(query.at(index)));
 
-		const Register* attributeRegister = getRegister(r.selections.at(i).first);
+		//apply selections
+		const Register* attributeRegister = getRegister(
+				r.selections.at(i).first);
 
 		Register* constantRegister = new Register();
-		constantRegister ->setString(r.selections.at(i).second.value);
+		constantRegister->setString(r.selections.at(i).second.value);
 
-		unique_ptr<Selection> select(new Selection(move(tableS),attributeRegister,constantRegister));
+		unique_ptr<Selection> select(
+				new Selection(move(tableS), attributeRegister,
+						constantRegister));
 		//replace base relation with relations after selection
 		query.at(index) = move(select);
 
@@ -122,56 +129,49 @@ void ExecutionPlan::generateExecutionPlan() {
 	vector<unique_ptr<Operator>> cross;
 	cross.push_back(move(query.at(0)));
 
-	for(unsigned int i=1; i<query.size(); i++){
+	for (unsigned int i = 1; i < query.size(); i++) {
 		//canonical translation of cross product
 
-		cross.push_back(unique_ptr<CrossProduct>(new CrossProduct(move(cross.at(i-1)), move(query.at(i)))));
+		cross.push_back(
+				unique_ptr<CrossProduct>(
+						new CrossProduct(move(cross.at(i - 1)),
+								move(query.at(i)))));
 	}
 
-
-
-	//Join
+	//do Join
 	vector<unique_ptr<Operator>> join;
-	join.push_back(move(cross.at(0)));
-	for(unsigned int i=0; i<r.joinConditions.size(); i++){
-		const Register* joinAttribute1 = getRegister(r.joinConditions.at(i).first);
-		const Register* joinAttribute2 = getRegister(r.joinConditions.at(i).second);
-		join.push_back(unique_ptr<Selection> (new Selection(move(join.at(i)), joinAttribute1, joinAttribute2)));
+	join.push_back(move(cross.at(cross.size() - 1)));
+	for (unsigned int i = 0; i < r.joinConditions.size(); i++) {
+		const Register* joinAttribute1 = getRegister(
+				r.joinConditions.at(i).first);
+		const Register* joinAttribute2 = getRegister(
+				r.joinConditions.at(i).second);
+
+		join.push_back(
+				unique_ptr<Selection>(
+						new Selection(move(join.at(i)), joinAttribute1,
+								joinAttribute2)));
 	}
 
 
-	cout << "Join" << endl;
-		//Print result
-	int size = join.size();
-	cout << "size: "<< size << endl;
-
-	//join.at(2).reset();
-	//cout << "reset ok " << endl;
-
-	unique_ptr<Operator> result = move(join.at(0));
-
-		Printer out1(move(result));
-		   out1.open();
-		   while (out1.next());
-		   out1.close();
-cout << "Print end" << endl;
-
+	//do projection
 	vector<const Register*> projectionRegister;
-	for(unsigned int i=0; i<r.projections.size(); i++){
+	for (unsigned int i = 0; i < r.projections.size(); i++) {
 		projectionRegister.push_back(getRegister(r.projections.at(i)));
-
 	}
-	unique_ptr<Projection> projection = unique_ptr<Projection>(new Projection(move(cross.at(cross.size()-1)), projectionRegister));
+	unique_ptr<Projection> projection = unique_ptr<Projection>(
+			new Projection(move(join.at(join.size() - 1)), projectionRegister));
 
-/*
-	cout << "Projection" << endl;
+
+
 	//Print result
+	cout << "\nResult of the query: "<<endl;
 	Printer out(move(projection));
 
-	   out.open();
-	   while (out.next());
-	   out.close();
-	   */
+	out.open();
+	while (out.next());
+	out.close();
+
 }
 
 bool vectorContainsAttribute(vector<SQLParser::RelationAttribute> vector,
@@ -187,14 +187,17 @@ bool vectorContainsAttribute(vector<SQLParser::RelationAttribute> vector,
 
 }
 
-const Register* ExecutionPlan::getRegister (SQLParser::RelationAttribute attr){
+const Register* ExecutionPlan::getRegister(SQLParser::RelationAttribute attr) {
 	const Register* reg = NULL;
-	for(unsigned int i=0; i<attributes.size(); i++){
-		if(attributes.at(i).first.getName().compare(attr.getName())){
-			reg=attributes.at(i).second;
+	for (unsigned int i = 0; i < attributes.size(); i++) {
+		if (attributes.at(i).first.getName().compare(attr.getName()) == 0) {
+			reg = attributes.at(i).second;
 			break;
 		}
 	}
 
+	if (reg == NULL) {
+		throw "Error! Register not found";
+	}
 	return reg;
 }
